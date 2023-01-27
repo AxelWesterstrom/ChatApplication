d12efa568fc302f0a8a4fb46d28f57a54ac0f597
Axel Westerstrom <axel.westerstrom@gmail.com>
2023-01-27 09:44:20 +0100

    can now invite people in the database

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,14 +1,47 @@
-import { Col, Modal, Button, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Modal, Button, ListGroup, ListGroupItem } from "react-bootstrap";
 import React from "react";
 import "../css/chatCard.css";
 import { useStates } from "../assets/states";
-import ModalListItem from "./ModalListItem";
 
 function ChatCard(user) {
   const [show, setShow] = React.useState(false);
   const handleOpen = () => setShow(true);
   const handleClose = () => setShow(false);
   let myRooms = useStates("myChatRooms");
+
+  const MyListGroup = () => (
+    <ListGroup>
+      {myRooms.rooms.map((room) => (
+        <ListGroupItem key={room.roomId} onClick={() => inviteUser(room)}>
+          {room.chatRoomName}
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+
+  async function getUserInfo() {
+    let userToInvite = await (
+      await fetch(`/api/users?username=${user.username}`)
+    ).json();
+    return userToInvite;
+  }
+
+  async function inviteUser(room) {
+    let toInvite = await getUserInfo();
+    console.log(room);
+    await (
+      await fetch("/api/members", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          roomId: room.roomId,
+          userId: toInvite[0].id,
+          owner: 0,
+          pending: 1,
+        }),
+      })
+    ).json();
+  }
 
   return (ChatCard = (
     <>
@@ -18,12 +51,7 @@ function ChatCard(user) {
       <Modal show={show} onClose={handleClose}>
         <Modal.Header>Invite to room</Modal.Header>
         <Modal.Body>
-          <ListGroup>
-            {" "}
-            {myRooms.rooms.map((room) => (
-              <ModalListItem key={room.roomId} user={user.username} {...room} />
-            ))}
-          </ListGroup>
+          <ListGroup>{MyListGroup()}</ListGroup>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={handleClose}>Close</Button>
@@ -34,3 +62,7 @@ function ChatCard(user) {
 }
 
 export default ChatCard;
+//  {" "}
+//             {myRooms.rooms.map((room) => (
+//               <ModalListItem key={room.roomId} user={user.username} {...room} />
+//             ))}
